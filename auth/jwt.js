546a4c85f67d2dfacfd440714ccac489937f5fe4
haris <haris.markcoders@gmail.com>
2024-03-06 20:03:08 +0500

    vercel build 5

--- auth/jwt.js
+++ auth/jwt.js
@@ -1,6 +1,6 @@
 const dotenv=require('dotenv').config()
 const jwt = require('jsonwebtoken');
-const {User}=require('./models/User')
+const {User}=require('../models/User')
 const stripe=require('stripe')(process.env.STRIPE_KEY)
 
 function generateAccessToken(user) {
